b8add48c6bb6aacd751237eaf0e5d71b60db90ff
rickmaster2008 <ricardo.cotillo@gmail>
2021-05-01 10:01:48 -0500

    added cors middleware

--- router/base.router.go
+++ router/base.router.go
@@ -2,12 +2,14 @@ package router
 
 import (
 	"cotillo.tech/inei_api/controllers"
+	"cotillo.tech/inei_api/middlewares"
 	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
 func InitRouter(db *gorm.DB) *mux.Router {
 	r := mux.NewRouter()
+	r.Use(middlewares.CorsMiddleware)
 	dc := controllers.DepartamentController{DB: db}
 	pc := controllers.ProductController{DB: db}
 	r.HandleFunc("/departments/", dc.Index).Methods("GET")
